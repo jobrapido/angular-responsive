d5deee58c79cded3075e67b06d9d07e937128bdc
Andrea Vaghi <andrea.vaghi@jobrapido.com>
2015-02-26 14:09:53 +0100

    modify script to adapt it for jobrapido website

--- src/responsive-directive.js
+++ src/responsive-directive.js
@@ -1,166 +1,140 @@
-(function ( angular ) {
-    'use strict';
+(function(angular) {
+  'use strict';
 
-    angular
-        .module('angular-responsive', [])
-        .provider('responsiveHelper', ["$windowProvider", function ($windowProvider)
-        {
-            // Gather winWidth based in Twitter BootStrap http://getbootstrap.com/css/#grid-media-queries
+  angular
+    .module('angular-responsive', [])
+    .provider('responsiveHelper', ['$windowProvider', function($windowProvider) {
 
-            var $window  = $windowProvider.$get();
-            // is better get first the innerWitdh that will not include a lateral panel
-            // like the console inspector, bookmarks, etc
-            var winWidth = $window.innerWidth || $window.outerWidth;
-            var helper   = {
+      var $window = $windowProvider.$get();
+      var winWidth = $window.innerWidth || $window.outerWidth;
+      var helper = {
+        //isSmartDevice : isSmartDevice( $window ),
+        isSmall: function() {
+          return winWidth < 768;
+        },
+        isMedium: function() {
+          return winWidth >= 768 && winWidth <= 1024;
+        },
+        isLarge: function() {
+          return winWidth > 1024;
+        }
+      };
 
-                //isSmartDevice : isSmartDevice( $window ),
-                isXs: function () { return winWidth < 768; },
-                isSm: function () { return winWidth >= 768 && winWidth < 992; },
-                isMd: function () { return winWidth >= 992 && winWidth < 1200; },
-                isLg: function () { return winWidth >= 1200; }
+      // Publish accessor function...
 
-            };
+      this.$get = function() {
+        return helper;
+      };
+    }])
 
-            // Publish accessor function...
+  /**
+   * Extra small devices Phones (<768px)
+   */
+  .directive('jrResponsiveSmall', ['responsiveHelper', function(responsiveHelper) {
+    return {
+      restrict: 'EAC',
+      transclude: 'element',
+      template: '<div></div>',
+      compile: buildCompileFn('jrResponsiveSmall', responsiveHelper.isSmall)
+    };
+  }])
 
-            this.$get = function() {
-                return helper;
-            };
-        }])
+  /**
+   * Medium devices Desktops (≥992px)
+   */
+  .directive('jrResponsiveMedium', ['responsiveHelper', function(responsiveHelper) {
+    return {
+      restrict: 'EAC',
+      transclude: 'element',
+      template: '<div></div>',
+      compile: buildCompileFn('jrResponsiveMedium', responsiveHelper.isMedium)
+    };
+  }])
 
-        /**
-         * Extra small devices Phones (<768px)
-         */
-        .directive('arXs', ['responsiveHelper', function (responsiveHelper)
-        {
-            return {
-                restrict    : "EAC",
-                transclude  : 'element',
-                template    : '<div></div>',
-                compile     : buildCompileFn( 'arXs', responsiveHelper.isXs )
-            };
-        }])
+  /**
+   * Large devices Desktops (≥1200px)
+   */
+  .directive('jrResponsiveLarge', ['responsiveHelper', function(responsiveHelper) {
+    return {
+      restrict: 'EAC',
+      transclude: 'element',
+      template: '<div></div>',
+      compile: buildCompileFn('jrResponsiveLarge', responsiveHelper.isLarge)
+    };
+  }])
 
-        /**
-         * Small devices Tablets (≥768px)
-         */
-        .directive('arSm', ['responsiveHelper', function (responsiveHelper)
-        {
-            return {
-                restrict    : "EAC",
-                transclude  : 'element',
-                template    : '<div></div>',
-                compile     : buildCompileFn( 'arSm', responsiveHelper.isSm )
-            };
-        }])
+  /**
+   * Does the with a match user-specified combination (0..4)
+   */
+  .directive('arResponsive', ['responsiveHelper', function(responsiveHelper) {
+    return {
+      restrict: 'EAC',
+      transclude: 'element',
+      template: '<div></div>',
+      compile: buildCompileFn('jrResponsive', checkAllTypes(responsiveHelper))
+    };
+  }]);
 
-        /**
-         * Medium devices Desktops (≥992px)
-         */
-        .directive('arMd', ['responsiveHelper', function (responsiveHelper)
-        {
-            return {
-                restrict    : "EAC",
-                transclude  : 'element',
-                template    : '<div></div>',
-                compile     : buildCompileFn( 'arMd', responsiveHelper.isMd )
-            };
-        }])
+  /**
+   * Partial application for DRY construction of a directive compile function
+   */
+  function buildCompileFn(responsiveType, verifyFn) {
+    return function compile(element, attr, transclude) {
+      return function postLink(scope, element, attr) {
+        var childElement, childScope,
+          config = scope.$eval(attr[responsiveType]),
+          unwatch = scope.$watch(config, function() {
+            // attribute changed, delete existing element & $scope
 
-        /**
-         * Large devices Desktops (≥1200px)
-         */
-        .directive('arLg', ['responsiveHelper', function (responsiveHelper)
-        {
-            return {
-                restrict    : "EAC",
-                transclude  : 'element',
-                template    : '<div></div>',
-                compile     : buildCompileFn( 'arLg', responsiveHelper.isLg )
-            };
-        }])
+            if (childElement) {
+              childElement.remove();
+              childScope.$destroy();
+              childElement = undefined;
+              childScope = undefined;
+            }
 
-        /**
-         * Does the with a match user-specified combination (0..4)
-         */
-        .directive('arResponsive', ['responsiveHelper', function (responsiveHelper)
-        {
-            return {
-                restrict    : "EAC",
-                transclude  : 'element',
-                template    : '<div></div>',
-                compile     : buildCompileFn( 'arResponsive', checkAllTypes(responsiveHelper) )
-            };
-        }]);
+            if (verifyFn(config)) {
+              // Create a new element and $scope...
 
-    /**
-     * Partial application for DRY construction of a directive compile function
-     */
-    function buildCompileFn(responsiveType, verifyFn )
-    {
-        return function compile(element, attr, transclude)
-        {
-            return function postLink(scope, element, attr)
-            {
-                var childElement, childScope,
-                    config  = scope.$eval( attr[responsiveType] ),
-                    unwatch = scope.$watch( config, function ()
-                    {
-                        // attribute changed, delete existing element & $scope
+              childScope = scope.$new();
+              childElement = transclude(childScope, function(clone) {
+                element.after(clone);
+              });
+            }
+          });
 
-                        if (childElement) {
-                            childElement.remove();
-                            childScope.$destroy();
-                            childElement = undefined;
-                            childScope = undefined;
-                        }
+        // Fix memory leak an remove watcher when element/directive is released
 
-                        if ( verifyFn(config) )
-                        {
-                            // Create a new element and $scope...
+        scope.$on("$destroy", unwatch);
+      };
+    };
+  }
 
-                            childScope = scope.$new();
-                            childElement = transclude(childScope, function (clone) {
-                                element.after(clone);
-                            });
-                        }
-                    });
+  /**
+   * Partial application for DRY construction of function to scan of any valid responsive types
+   */
+  function checkAllTypes(responsiveHelper) {
+    return function(deviceTypes) {
+      return (deviceTypes['small'] && responsiveHelper.isSmall()) ||
+        (deviceTypes['medium'] && responsiveHelper.isMedium()) ||
+        (deviceTypes['large'] && responsiveHelper.isLarge()) || false;
+    };
+  }
 
-                // Fix memory leak an remove watcher when element/directive is released
+  /**
+   * Scan to determine if current window is hosted within a `smart` device
+   * @param $window
+   * @returns {boolean}
+   */
+  /*
+  function isSmartDevice( $window )
+  {
+      // Adapted from http://www.detectmobilebrowsers.com
+      var ua = $window['navigator']['userAgent'] || $window['navigator']['vendor'] || $window['opera'];
 
-                scope.$on( "$destroy", unwatch );
-            };
-        };
-    }
+      // Checks for iOs, Android, Blackberry, Opera Mini, and Windows mobile devices
+      return (/iPhone|iPod|iPad|Silk|Android|BlackBerry|Opera Mini|IEMobile/).test(ua);
+  }
+  */
 
-    /**
-     * Partial application for DRY construction of function to scan of any valid responsive types
-     */
-    function checkAllTypes(responsiveHelper)
-    {
-        return function( deviceTypes )
-        {
-            return  ( deviceTypes['xs']  && responsiveHelper.isXs()  ) ||
-                ( deviceTypes['sm']  && responsiveHelper.isSm()  ) ||
-                ( deviceTypes['md']  && responsiveHelper.isMd()  ) ||
-                ( deviceTypes['lg'] && responsiveHelper.isLg() ) || false;
-        };
-    }
-
-    /**
-     * Scan to determine if current window is hosted within a `smart` device
-     * @param $window
-     * @returns {boolean}
-     */
-    /*
-    function isSmartDevice( $window )
-    {
-        // Adapted from http://www.detectmobilebrowsers.com
-        var ua = $window['navigator']['userAgent'] || $window['navigator']['vendor'] || $window['opera'];
-
-        // Checks for iOs, Android, Blackberry, Opera Mini, and Windows mobile devices
-        return (/iPhone|iPod|iPad|Silk|Android|BlackBerry|Opera Mini|IEMobile/).test(ua);
-    }
-    */
-
-})( window.angular );
+})(window.angular);
